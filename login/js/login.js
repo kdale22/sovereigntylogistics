5e3909d8700f967a3d81a91dbfd395472d928f02
dynamicdreamz1 <mehtabhaumil@gmail.com>
2026-08-13 12:42:28 +0530

    login file changes

--- login/js/login.js
+++ login/js/login.js
@@ -1,11 +1,22 @@
-if (window.netlifyIdentity) {
-    window.netlifyIdentity.on("init", user => {
+document.addEventListener("DOMContentLoaded", function () {
+
+    if (!window.netlifyIdentity) {
+        console.error("Netlify Identity is not loaded.");
+        return;
+    }
+
+    window.netlifyIdentity.on("init", function (user) {
+
         if (user) {
             window.location.href = "/secure/";
         }
+
     });
 
-    window.netlifyIdentity.on("login", () => {
+    window.netlifyIdentity.on("login", function () {
+
         window.location.href = "/secure/";
+
     });
-}
+
+});
