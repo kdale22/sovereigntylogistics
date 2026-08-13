1729b6a165d192f86ff51d222feaaae8416b2ed0
dynamicdreamz1 <mehtabhaumil@gmail.com>
2026-08-13 14:46:42 +0530

    update recovery js

--- js/recovery-redirect.js
+++ js/recovery-redirect.js
@@ -1,7 +1,22 @@
 (function () {
     const hash = window.location.hash;
 
-    if (hash && hash.indexOf("#recovery_token=") === 0) {
+    if (!hash) {
+        return;
+    }
+
+    const supportedTokens = [
+        "#recovery_token=",
+        "#invite_token=",
+        "#confirmation_token=",
+        "#email_change_token="
+    ];
+
+    const isAuthToken = supportedTokens.some(function (token) {
+        return hash.startsWith(token);
+    });
+
+    if (isAuthToken) {
         window.location.replace("/login/" + hash);
     }
 })();
